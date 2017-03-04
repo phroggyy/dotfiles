5593d71022783172ca54bbee237affb66688eb93
Leo Sjöberg <leo.sjoberg@gmail.com>
2017-03-04 13:20:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+These are my dotfiles. They are sacred to me.
---
+++ new-setup.sh
@@ -0,0 +1,20 @@
+#################################
+### TO DO BEFORE RUNNING THIS ###
+#################################
+# 1. Install iTerm2 https://www.iterm2.com
+# 2. Make sure you run xcode-select --install before running this
+# 3. Install Sublime Text 3 https://www.sublimetext.com
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Yarn
+brew install yarn
+
+# Configure Git
+git config --global user.email "leo.sjoberg@gmail.com"
+git config --global user.name "Leo Sjöberg"
+git config --global push.default current    # This will make sure we automatically create the remote branch without -u for new branches
