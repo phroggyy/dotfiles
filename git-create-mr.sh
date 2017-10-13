033356dc0e9452e66b4e46b7c1f6cdee0e217acf
Leo Sjöberg <leo.sjoberg@gmail.com>
2017-10-13 15:28:30 +0100

    Update MR script

--- git-create-mr.sh
+++ git-create-mr.sh
@@ -1,9 +1,14 @@
 # Adapted from https://github.com/lukewaite/dotfiles/blob/master/git-create-mr.sh
+#!/usr/bin/env bash
 
 me=${1:-origin}
 branch=`git rev-parse --abbrev-ref HEAD`
 
-remote=`git remote -v | grep "(push)$" | grep $remoteName`
+originalBranch=`git branch-name`
+branch="${originalBranch}-staging"
+commitHash=`git rev-parse HEAD`
+
+remote=`git remote -v | grep "(push)$" | grep origin`
 regex="origin[[:space:]]+git@([A-Za-z\.]+)[\:|\/](.*)/(.*).git"
 
 if [[ $remote =~ $regex ]]; then
@@ -15,19 +20,15 @@ else
     exit 1
 fi
 
-
-target="master"
-
-if [[ "$branch" == *STAGING ]]; then
-    target="staging"
+if git show-ref --heads --quiet $branch; then
+    git branch -D $branch
+    force="--force"
 fi
 
-if [[ "$server" == github.com ]]; then
-    open "https://$server/$group/$project/compare/$target...$branch"
-else
-    if [[ "$target" == master ]]; then
-        open "https://$server/$group/$project/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5B"
-    else
-        open "https://$server/$group/$project/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5Btarget_branch%5D=$target"
-    fi
+git checkout staging && git pull && git checkout -b $branch && git cherry-pick $commitHash && git push -u origin $force
+
+if [[ $force = "" ]]; then
+    open "https://$server/$group/$project/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5Btarget_branch%5D=staging"
 fi
+
+git checkout $originalBranch
